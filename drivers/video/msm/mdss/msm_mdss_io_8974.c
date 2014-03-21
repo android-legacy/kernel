6e7900141e38c9e1f358706ef214689c946460d9
Mao Flynn <Flynn@codeaurora.org>
2014-03-21 14:28:05 +0800

    msm: mdss: support LDO mode for DSI PHY regulator
    DSI PHY supports two regulator modes: LDO and DCDC.
    While DCDC offers power efficiency, but it requires an
    external inductor. So it is required to enable LDO mode
    to save the extra PCB board cost.
    
    Change-Id: I156ba8af7857743db5373a9e3f79d948cf9e1427
    Signed-off-by: Mao Flynn <Flynn@codeaurora.org>

--- drivers/video/msm/mdss/msm_mdss_io_8974.c
+++ drivers/video/msm/mdss/msm_mdss_io_8974.c
@@ -960,9 +960,9 @@ bool __mdss_dsi_clk_enabled(struct mdss_dsi_ctrl_pdata *ctrl, u8 clk_type)
 	/* Regulator ctrl 4 */
 	MIPI_OUTP((temp_ctrl->phy_io.base) + 0x290, pd->regulator[4]);
 
-	/* LDO ctrl 0 */
-	if ((ctrl_pdata->panel_data).panel_info.pdest == DISPLAY_1)
-		MIPI_OUTP((ctrl_pdata->phy_io.base) + 0x1dc, 0x00);
+	/* LDO ctrl */
+	if (pd->reg_ldo_mode)
+		MIPI_OUTP((ctrl_pdata->phy_io.base) + 0x1dc, 0x25);
 	else
 		MIPI_OUTP((ctrl_pdata->phy_io.base) + 0x1dc, 0x00);
 
