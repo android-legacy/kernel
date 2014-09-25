c48b12b7f936af5fe7d268fffd898d42990c3374
Ingrid Gallardo <ingridg@codeaurora.org>
2014-09-24 17:08:51 -0700

    msm: mdss: add fudge factor to single pipe scenarios
    Add fudge factor to ib vote for scenarios with high
    bandwidth request whenever a single pipe is present,
    this is to account for inefficiencies as recommended.
    
    Change-Id: I6bdfb8d71c61f58a26fae95bdbd5bc9a2d043957
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -69,6 +69,8 @@
 #define PERF_CALC_PIPE_SINGLE_LAYER	BIT(1)
 #define PERF_CALC_PIPE_CALC_SMP_SIZE	BIT(2)
 
+#define PERF_SINGLE_PIPE_BW_FLOOR 1200000000
+
 enum mdss_mdp_perf_state_type {
 	PERF_SW_COMMIT_STATE = 0,
 	PERF_HW_MDP_STATE,
@@ -163,9 +165,7 @@ enum mdss_mdp_wb_ctl_type {
 };
 
 enum mdss_mdp_bw_vote_mode {
-	MDSS_MDP_BW_MODE_NONE = 0,
-	MDSS_MDP_BW_MODE_UHD = BIT(0),
-	MDSS_MDP_BW_MODE_QHD = BIT(1),
+	MDSS_MDP_BW_MODE_SINGLE_LAYER,
 	MDSS_MDP_BW_MODE_MAX
 };
 
@@ -175,7 +175,7 @@ struct mdss_mdp_perf_params {
 	u32 prefill_bytes;
 	u64 bw_ctl;
 	u32 mdp_clk_rate;
-	u32 bw_vote_mode;
+	DECLARE_BITMAP(bw_vote_mode, MDSS_MDP_BW_MODE_MAX);
 };
 
 struct mdss_mdp_ctl {
