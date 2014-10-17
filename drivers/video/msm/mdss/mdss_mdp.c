fc33c0a19c8441751d2d16b0ec1bf089ca76aae6
Siddhartha Agrawal <agrawals@codeaurora.org>
2014-10-16 18:17:55 -0700

    msm: mdss: Increase IB/AB votes during cont splash screen
    We now support high resolutions like 4K that need higher b/w votes.
    Increase b/w vote to 2GBps for the duration of the continuous splash
    screen logo.
    
    Change-Id: I2c73e98ea769b252bee51d953d7cdc24f2b6668a
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -82,8 +82,8 @@ struct msm_mdp_interface mdp5 = {
 #define DEFAULT_TOTAL_VIG_PIPES 3
 #define DEFAULT_TOTAL_DMA_PIPES 2
 
-#define IB_QUOTA 800000000
-#define AB_QUOTA 800000000
+#define IB_QUOTA 2000000000
+#define AB_QUOTA 2000000000
 
 #define MAX_AXI_PORT_COUNT 3
 
