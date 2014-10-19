6cc24d33115ae9e1556415d30949d6b94d8292cb
erikcas <erikcas1972@gmail.com>
2014-10-19 11:14:46 +0200

    import 18.3.A.0.31, 18.3.B.0.31, and 18.3.C.0.37
    * Added CONFIG_SONY_EAGLE
    * Replaced Sony ifdefs
    * Removed SSM and SONY RIC
    
    Change-Id: I9fe65abe2340748058b8f72ab325bc113d634ddf
    
    Conflicts:
    	Documentation/devicetree/bindings/arm/msm/cpr-regulator.txt
    	arch/arm/boot/dts/msm-pm8226.dtsi
    	arch/arm/boot/dts/msm8226-camera-sensor-mtp.dtsi
    	arch/arm/boot/dts/msm8226-v2.dtsi
    	arch/arm/boot/dts/msm8226.dtsi
    	arch/arm/boot/dts/msm8926.dtsi
    	arch/arm/mach-msm/Makefile.boot
    	arch/arm/mach-msm/board-8226.c
    	arch/arm/mach-msm/clock-8226.c
    	arch/arm/mach-msm/cpr-regulator.c
    	drivers/gpu/msm/adreno.h
    	drivers/gpu/msm/adreno_a3xx.c
    	drivers/gpu/msm/adreno_drawctxt.c
    	drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
    	drivers/media/platform/msm/camera_v2/sensor/gc0339.c
    	drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
    	drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
    	drivers/video/msm/mdss/mdss_dsi.c
    	sound/soc/codecs/wcd9306.c

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -20,13 +20,21 @@
 #include <mach/rpm-regulator-smd.h>
 #include <linux/regulator/consumer.h>
 
+<<<<<<< HEAD
 /*#define CONFIG_MSMB_CAMERA_DEBUG*/
+=======
+#include "eeprom/msm_eeprom.h"
+
+>>>>>>> 2e33fc5... import 18.3.A.0.31, 18.3.B.0.31, and 18.3.C.0.37
 #undef CDBG
 #ifdef CONFIG_MSMB_CAMERA_DEBUG
 #define CDBG(fmt, args...) pr_err(fmt, ##args)
 #else
 #define CDBG(fmt, args...) do { } while (0)
 #endif
+/**/
+#define CCI_camera
+/**/
 
 static int32_t msm_camera_get_power_settimgs_from_sensor_lib(
 	struct msm_camera_power_ctrl_t *power_info,
@@ -129,6 +137,25 @@ static int32_t msm_sensor_get_dt_data(struct device_node *of_node,
 		pr_err("%s failed %d\n", __func__, __LINE__);
 		goto FREE_SENSORDATA;
 	}
+	/**/
+	if(strcmp("imx134",s_ctrl->sensordata->sensor_name)==0) {
+		CDBG("%s:Main camera source no. is : %d\n", __func__,
+			cci_camera_source);
+		if(cci_camera_source == 1) {
+			s_ctrl->sensordata->sensor_name = "imx134_Sony";/*Bug1647,Guanyi,QC patch0118*/
+			CDBG("This is original source camera module-SONY\n");
+		}
+		else if(cci_camera_source == 2) {
+			s_ctrl->sensordata->sensor_name = "imx134li_QC";
+			CDBG("This is second source camera module-LiteON\n");
+		}
+		else {
+			pr_err("%s Invalid camera source number:%d\n", __func__,cci_camera_source);
+		}
+	}
+	pr_err("%s:[Mark] sensor-name: %s\n", __func__,
+			s_ctrl->sensordata->sensor_name);
+	/**/
 
 	rc = of_property_read_u32(of_node, "qcom,cci-master",
 		&s_ctrl->cci_i2c_master);
@@ -462,11 +489,24 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 
 int msm_sensor_match_id(struct msm_sensor_ctrl_t *s_ctrl)
 {
+<<<<<<< HEAD
 	int rc = 0;
 	uint16_t chipid = 0;
 	struct msm_camera_i2c_client *sensor_i2c_client;
 	struct msm_camera_slave_info *slave_info;
 	const char *sensor_name;
+=======
+	int32_t index = 0;
+        
+	int32_t gpiotestnum = 0;
+	struct msm_sensor_power_setting_array *power_setting_array = NULL;
+	struct msm_sensor_power_setting *power_setting = NULL;
+	struct msm_camera_sensor_board_info *data = s_ctrl->sensordata;
+	s_ctrl->stop_setting_valid = 0;
+
+	CDBG("%s:%d\n", __func__, __LINE__);
+	power_setting_array = &s_ctrl->power_setting_array;
+>>>>>>> 2e33fc5... import 18.3.A.0.31, 18.3.B.0.31, and 18.3.C.0.37
 
 	if (!s_ctrl) {
 		pr_err("%s:%d failed: %p\n",
@@ -477,11 +517,74 @@ int msm_sensor_match_id(struct msm_sensor_ctrl_t *s_ctrl)
 	slave_info = s_ctrl->sensordata->slave_info;
 	sensor_name = s_ctrl->sensordata->sensor_name;
 
+<<<<<<< HEAD
 	if (!sensor_i2c_client || !slave_info || !sensor_name) {
 		pr_err("%s:%d failed: %p %p %p\n",
 			__func__, __LINE__, sensor_i2c_client, slave_info,
 			sensor_name);
 		return -EINVAL;
+=======
+	for (index = (power_setting_array->size - 1); index >= 0; index--) {
+		CDBG("%s index %d\n", __func__, index);
+		power_setting = &power_setting_array->power_setting[index];
+		CDBG("%s type %d\n", __func__, power_setting->seq_type);
+		switch (power_setting->seq_type) {
+		case SENSOR_CLK:
+			msm_cam_clk_enable(s_ctrl->dev,
+				&s_ctrl->clk_info[0],
+				(struct clk **)&power_setting->data[0],
+				s_ctrl->clk_info_size,
+				0);
+			break;
+		case SENSOR_GPIO:
+			if (power_setting->seq_val >= SENSOR_GPIO_MAX ||
+				!data->gpio_conf->gpio_num_info) {
+				pr_err("%s gpio index %d >= max %d\n", __func__,
+					power_setting->seq_val,
+					SENSOR_GPIO_MAX);
+				continue;
+			}
+                        
+			gpiotestnum = data->gpio_conf->gpio_num_info->gpio_num
+					[power_setting->seq_val];
+			if((gpiotestnum == 69) && (gpio69_count == 2)){
+				CDBG("[VY5X][CTS]Avoid sub camera preview fail in CTS\n");
+			}
+			else {
+			gpio_set_value_cansleep(
+				data->gpio_conf->gpio_num_info->gpio_num
+				[power_setting->seq_val], GPIOF_OUT_INIT_LOW);
+			}
+                        
+			break;
+		case SENSOR_VREG:
+			if (power_setting->seq_val >= CAM_VREG_MAX) {
+				pr_err("%s vreg index %d >= max %d\n", __func__,
+					power_setting->seq_val,
+					SENSOR_GPIO_MAX);
+				continue;
+			}
+			msm_camera_config_single_vreg(s_ctrl->dev,
+				&data->cam_vreg[power_setting->seq_val],
+				(struct regulator **)&power_setting->data[0],
+				0);
+			break;
+		case SENSOR_I2C_MUX:
+			if (data->i2c_conf && data->i2c_conf->use_i2c_mux)
+				msm_sensor_disable_i2c_mux(data->i2c_conf);
+			break;
+		default:
+			pr_err("%s error power seq type %d\n", __func__,
+				power_setting->seq_type);
+			break;
+		}
+		if (power_setting->delay > 20) {
+			msleep(power_setting->delay);
+		} else if (power_setting->delay) {
+			usleep_range(power_setting->delay * 1000,
+				(power_setting->delay * 1000) + 1000);
+		}
+>>>>>>> 2e33fc5... import 18.3.A.0.31, 18.3.B.0.31, and 18.3.C.0.37
 	}
 
 	rc = sensor_i2c_client->i2c_func_tbl->i2c_read(
@@ -1124,6 +1227,61 @@ static struct msm_camera_i2c_fn_t msm_sensor_qup_func_tbl = {
 		msm_camera_qup_i2c_write_table_w_microdelay,
 	.i2c_write_conf_tbl = msm_camera_qup_i2c_write_conf_tbl,
 };
+/**/
+static ssize_t CheckCameraID_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+  uint16_t rc;
+  uint16_t chipid = 0;
+
+  struct msm_sensor_ctrl_t *s_ctrl = (struct msm_sensor_ctrl_t *)dev->driver->of_match_table->data;
+
+  CDBG("[Vince Debug] Enter Ping Function \t%s:%d \n", __func__, __LINE__);//CDBG
+
+  if(s_ctrl)
+  {
+      //CDBG("[Vince Debug] s_ctrl get -start power up %s:%d \n", __func__, __LINE__);
+      rc= s_ctrl->func_tbl->sensor_power_up(s_ctrl);
+      if (rc < 0)
+      {
+          pr_err("%s %s power up failed\n", __func__,
+          s_ctrl->sensordata->sensor_name);
+          pr_err("[Vince Debug] power up fail %s:%d \n", __func__, __LINE__);
+          return sprintf(buf, "ID: error 0x03 \n");
+      }
+      //CDBG("[Vince Debug] power up okay - start i2c tranfer %s:%d \n", __func__, __LINE__);
+      if(strcmp("SKUAA_ST_gc0339",s_ctrl->sensordata->sensor_name))
+      {//for general
+          rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_read(
+          s_ctrl->sensor_i2c_client,
+          s_ctrl->sensordata->slave_info->sensor_id_reg_addr,
+          &chipid, MSM_CAMERA_I2C_WORD_DATA);
+      }
+      else
+      {//for gc0339
+          rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_read(
+          s_ctrl->sensor_i2c_client,
+          s_ctrl->sensordata->slave_info->sensor_id_reg_addr,
+          &chipid, MSM_CAMERA_I2C_BYTE_DATA);
+      }
+      if (rc < 0)
+      {
+          s_ctrl->func_tbl->sensor_power_down(s_ctrl);
+          pr_err("%s: %s: read id failed\n", __func__,s_ctrl->sensordata->sensor_name);
+          return sprintf(buf, "ID: error 0x01 \n");
+      }
+      //CDBG("[Vince Debug] check id okay -start power dwon %s:%d \n", __func__, __LINE__);
+      s_ctrl->func_tbl->sensor_power_down(s_ctrl);
+  }
+  else
+  {
+      pr_err("[Vince Debug] s_ctrl get fail %s:%d \n", __func__, __LINE__);
+      return sprintf(buf, "ID: error 0x02 \n");
+  }
+  CDBG("[Vince Debug] Exit Ping function \t%s:%d \n", __func__, __LINE__);
+  return sprintf(buf, "ID: 0x%x\n", chipid);
+}
+static DEVICE_ATTR(CheckCameraID, S_IRUSR | S_IWUSR, CheckCameraID_show, NULL);
+/**/
 
 int32_t msm_sensor_platform_probe(struct platform_device *pdev, void *data)
 {
@@ -1216,6 +1374,17 @@ int32_t msm_sensor_platform_probe(struct platform_device *pdev, void *data)
 
 	s_ctrl->func_tbl->sensor_power_down(s_ctrl);
 	CDBG("%s:%d\n", __func__, __LINE__);
+	/**/
+#ifdef CCI_camera
+  CDBG("[Vince Debug] Pin Function create Function Enter\t%s:%d\n", __func__, __LINE__);
+  {
+      if(device_create_file(s_ctrl->dev, &dev_attr_CheckCameraID))
+      {
+          pr_err("[Vince Debug] Ping Function create fail!!\t%s:%d\n", __func__, __LINE__);
+      }
+  }
+#endif
+		/**/
 	return rc;
 }
 
