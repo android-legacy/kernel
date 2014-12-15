aff250bfb0ecbd55636446b4a92bd2cd7954e6b8
erikcas <erikcas1972@gmail.com>
2014-12-15 18:58:53 +0100

    just checking
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    	drivers/video/msm/mdss/mdss_dsi_panel.c
    	drivers/video/msm/mdss/mdss_fb.c
    
    Change-Id: I86ddd27a792ead3ea03becdb596c3d235d9f3a89

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -684,8 +684,11 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 	 * Issue hardware reset line after enabling the DSI clocks and data
 	 * data lanes for LP11 init
 	 */
-	if (mipi->lp11_init)
+	if (pdata->panel_info.mipi.lp11_init)
+  {
+    pr_info("pdata->panel_info.mipi.lp11_init == 1");
 		mdss_dsi_panel_reset(pdata, 1);
+  }
 
 	if (mipi->init_delay)
 		usleep(mipi->init_delay);
@@ -1050,6 +1053,8 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 	return rc;
 }
 
+//Joker marked
+/*
 static struct device_node *mdss_dsi_pref_prim_panel(
 		struct platform_device *pdev)
 {
@@ -1064,6 +1069,7 @@ static struct device_node *mdss_dsi_pref_prim_panel(
 
 	return dsi_pan_node;
 }
+*/
 
 /**
  * mdss_dsi_find_panel_of_node(): find device node of dsi panel
@@ -1079,9 +1085,17 @@ static struct device_node *mdss_dsi_pref_prim_panel(
  *
  * returns pointer to panel node on success, NULL on error.
  */
+
+//S [VVVV] JackBB 2013/10/03
+#define LCD_ID_GPIO 23
+int g_mdss_dsi_lcd_id = 0;
+//E [VVVV] JackBB 2013/10/03
+
 static struct device_node *mdss_dsi_find_panel_of_node(
 		struct platform_device *pdev, char *panel_cfg)
 {
+//S [VVVV] JackBB 2013/10/03
+#ifndef CONFIG_SONY_EAGLE
 	int len, i;
 	int ctrl_id = pdev->id - 1;
 	char panel_name[MDSS_MAX_PANEL_LEN];
@@ -1137,6 +1151,43 @@ static struct device_node *mdss_dsi_find_panel_of_node(
 	}
 end:
 	dsi_pan_node = mdss_dsi_pref_prim_panel(pdev);
+#else
+  struct device_node *dsi_pan_node = NULL;
+  int status,rc;
+  rc = gpio_request(LCD_ID_GPIO, "disp_lcd_id");
+
+  rc = gpio_tlmm_config(GPIO_CFG(
+		  LCD_ID_GPIO,
+		  0,
+		  GPIO_CFG_INPUT,
+		  GPIO_CFG_NO_PULL,//GPIO_CFG_PULL_UP,///*GPIO_CFG_PULL_DOWN,*/
+		  GPIO_CFG_2MA),
+		  GPIO_CFG_ENABLE);
+
+  status = gpio_get_value(LCD_ID_GPIO);
+  g_mdss_dsi_lcd_id = status;
+  pr_info("%s: LCD_ID = %d\n", __func__, status);
+
+  if(g_mdss_dsi_lcd_id == 0)
+  {
+	  dsi_pan_node = of_parse_phandle(
+		  pdev->dev.of_node,
+		  "qcom,dsi-pref-prim-pan1", 0);
+  }
+  else
+  {
+	  dsi_pan_node = of_parse_phandle(
+		  pdev->dev.of_node,
+		  "qcom,dsi-pref-prim-pan2", 0);
+  }
+
+	if (!dsi_pan_node) {
+		pr_err("%s:can't find panel phandle\n",
+		       __func__);
+		return NULL;
+  }
+#endif
+//E [VVVV] JackBB 2013/10/03
 
 	return dsi_pan_node;
 }
@@ -1480,7 +1531,16 @@ int dsi_panel_device_register(struct device_node *pan_node,
 	if (!gpio_is_valid(ctrl_pdata->disp_en_gpio))
 		pr_err("%s:%d, Disp_en gpio not specified\n",
 						__func__, __LINE__);
-
+	} else {
+		rc = gpio_request(ctrl_pdata->disp_en_gpio, "disp_enable");
+		if (rc) {
+			pr_err("request reset gpio failed, rc=%d\n",
+			       rc);
+			gpio_free(ctrl_pdata->disp_en_gpio);
+			return -ENODEV;
+		}
+	}
+#if 0
 	if (pinfo->type == MIPI_CMD_PANEL) {
 		ctrl_pdata->disp_te_gpio = of_get_named_gpio(ctrl_pdev->dev.of_node,
 						"qcom,platform-te-gpio", 0);
@@ -1522,6 +1582,7 @@ int dsi_panel_device_register(struct device_node *pan_node,
 		pr_debug("%s: te_gpio=%d\n", __func__,
 					ctrl_pdata->disp_te_gpio);
 	}
+#endif // Mike
 
 	ctrl_pdata->rst_gpio = of_get_named_gpio(ctrl_pdev->dev.of_node,
 			 "qcom,platform-reset-gpio", 0);
