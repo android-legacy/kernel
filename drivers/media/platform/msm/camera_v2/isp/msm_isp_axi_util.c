1dec71a3199d4fe813dac6cb99689d4d49390391
erikcas <erikcas1972@gmail.com>
2014-12-17 15:06:21 +0100

    re-port camera
    Change-Id: I905d63776fed1e4a366486546609434aa32f80bd

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -503,10 +503,7 @@ void msm_isp_calculate_framedrop(
 
 	framedrop_period = msm_isp_get_framedrop_period(
 			stream_cfg_cmd->frame_skip_pattern);
-	/*QCT patch 20140627 S add*/
-	stream_info->frame_skip_pattern =
-		stream_cfg_cmd->frame_skip_pattern;
-	/*QCT patch 20140627 E add*/
+
 	if (stream_cfg_cmd->frame_skip_pattern == SKIP_ALL)
 		stream_info->framedrop_pattern = 0x0;
 	else
@@ -1117,16 +1114,8 @@ static int msm_isp_update_stream_bandwidth(struct vfe_device *vfe_dev)
 	if (num_pix_streams > 0)
 		total_pix_bandwidth = total_pix_bandwidth /
 			num_pix_streams * (num_pix_streams - 1) +
-<<<<<<< HEAD
-			/**/
-			//axi_data->src_info[VFE_PIX_0].pixel_clock *
-			//ISP_DEFAULT_FORMAT_FACTOR / ISP_Q2;
-			(unsigned long)axi_data->src_info[VFE_PIX_0].
-			pixel_clock * ISP_DEFAULT_FORMAT_FACTOR / ISP_Q2;
-=======
 			((unsigned long)axi_data->src_info[VFE_PIX_0].
 			pixel_clock) * ISP_DEFAULT_FORMAT_FACTOR / ISP_Q2;
->>>>>>> AU_LINUX_ANDROID_LNX.LA.3.5.2.2_RB1.04.04.02.087.007
 	total_bandwidth = total_pix_bandwidth + total_rdi_bandwidth;
 
 	rc = msm_isp_update_bandwidth(ISP_VFE0 + vfe_dev->pdev->id,
@@ -1365,13 +1354,9 @@ static int msm_isp_stop_axi_stream(struct vfe_device *vfe_dev,
 	cur_stream_cnt = msm_isp_get_curr_stream_cnt(vfe_dev);
 	if (cur_stream_cnt == 0) {
 		if (camif_update == DISABLE_CAMIF_IMMEDIATELY) {
-			vfe_dev->hw_info->vfe_ops.axi_ops.halt(vfe_dev, 1);/*QCT patch 20140627 modify*/
+			vfe_dev->hw_info->vfe_ops.axi_ops.halt(vfe_dev);
 		}
-<<<<<<< HEAD
-		vfe_dev->hw_info->vfe_ops.core_ops.reset_hw(vfe_dev, ISP_RST_SOFT, 1);/*QCT patch 20140627 modify*/
-=======
 		vfe_dev->hw_info->vfe_ops.core_ops.reset_hw(vfe_dev, ISP_RST_HARD);
->>>>>>> AU_LINUX_ANDROID_LNX.LA.3.5.2.2_RB1.04.04.02.087.007
 		vfe_dev->hw_info->vfe_ops.core_ops.init_hw_reg(vfe_dev);
 	}
 
