android-legacy/android_kernel_caf_msm8x26

port camera from eagle stock camera
Change-Id: Iedd25d58f39dddd79d37495924318a6b0390e00f

drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -109,7 +109,11 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 					i2c_byte2 = (value & 0xFF00) >> 8;
 				}
 			} else {
-				i2c_byte1 = (value & 0xFF00) >> 8;
+#ifdef CONFIG_MACH_SONY_EAGLE
+					i2c_byte1 = ((value & 0x0300) >> 8) | 0xF4;
+#else
+					i2c_byte1 = (value & 0xFF00) >> 8;
+#endif
 				i2c_byte2 = value & 0xFF;
 			}
 		} else {
@@ -711,11 +715,25 @@ static int msm_actuator_close(struct v4l2_subdev *sd,
 	struct v4l2_subdev_fh *fh) {
 	int rc = 0;
 	struct msm_actuator_ctrl_t *a_ctrl =  v4l2_get_subdevdata(sd);
+#ifdef CONFIG_MACH_SONY_EAGLE
+    int rc2 = 0;
+#endif
 	CDBG("Enter\n");
 	if (!a_ctrl) {
 		pr_err("failed\n");
 		return -EINVAL;
 	}
+#ifdef CONFIG_MACH_SONY_EAGLE
+	CDBG("back to DAC 0x0000!\n");
+	rc2 = a_ctrl->i2c_client.i2c_func_tbl->i2c_write(
+		&a_ctrl->i2c_client,
+		0x00,
+		0x00,
+		MSM_CAMERA_I2C_BYTE_DATA);
+	if (rc2 < 0) {
+		pr_err("i2c write error:%d\n", rc);
+	}
+#endif
 	if (a_ctrl->act_device_type == MSM_CAMERA_PLATFORM_DEVICE) {
 		rc = a_ctrl->i2c_client.i2c_func_tbl->i2c_util(
 			&a_ctrl->i2c_client, MSM_CCI_RELEASE);

drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
@@ -24,6 +24,10 @@
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
 #endif
 
+#ifdef CONFIG_MACH_SONY_EAGLE
+int cci_camera_source = 0;
+#endif
+
 DEFINE_MSM_MUTEX(msm_eeprom_mutex);
 
 int32_t msm_eeprom_config(struct msm_eeprom_ctrl_t *e_ctrl,
@@ -217,6 +221,18 @@ int32_t read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl)
 			}
 			memptr += emap[j].mem.valid_size;
 		}
+#ifdef CONFIG_MACH_SONY_EAGLE
+		/*Bug1095,guanyi,EEPROM S*/
+		if(j==0){/*After A1 for AWB and AF,A3 A5 A7 for lsc*/
+		eb_info->i2c_slaveaddr=0xA3;
+		e_ctrl->i2c_client.cci_client->sid = eb_info->i2c_slaveaddr >> 1;
+		}
+		else{
+		eb_info->i2c_slaveaddr=eb_info->i2c_slaveaddr+2;
+		e_ctrl->i2c_client.cci_client->sid = eb_info->i2c_slaveaddr >> 1;
+		}
+		//pr_err("new id =0x%x \n",e_ctrl->i2c_client.cci_client->sid);
+#endif
 		if (emap[j].pageen.valid_size) {
 			e_ctrl->i2c_client.addr_type = emap[j].pageen.addr_t;
 			rc = e_ctrl->i2c_client.i2c_func_tbl->i2c_write(
@@ -790,6 +806,14 @@ static int32_t msm_eeprom_platform_probe(struct platform_device *pdev)
 	int32_t j = 0;
 	uint32_t temp;
 
+#ifdef CONFIG_MACH_SONY_EAGLE
+/*AF DATA S*/
+	uint16_t infinity_dac_t;
+	uint16_t macro_dac_t;
+	uint16_t starting_dac_t;
+	uint16_t a1=0;
+	uint16_t a2=0;
+#endif
 	struct msm_camera_cci_client *cci_client = NULL;
 	struct msm_eeprom_ctrl_t *e_ctrl = NULL;
 	struct msm_eeprom_board_info *eb_info = NULL;
@@ -906,6 +930,29 @@ static int32_t msm_eeprom_platform_probe(struct platform_device *pdev)
 		pr_err("%s line %d\n", __func__, __LINE__);
 	for (j = 0; j < e_ctrl->num_bytes; j++)
 		CDBG("memory_data[%d] = 0x%X\n", j, e_ctrl->memory_data[j]);
+#ifdef CONFIG_MACH_SONY_EAGLE
+		infinity_dac_t=(uint16_t)(e_ctrl->memory_data[0xD0] << 8) |
+                  e_ctrl->memory_data[0xD1];
+		macro_dac_t=(uint16_t)(e_ctrl->memory_data[0xD4] << 8) |
+                  e_ctrl->memory_data[0xD5];
+	if ((e_ctrl->memory_data[0]==0x53)||(e_ctrl->memory_data[0]==0x73)){
+		cci_camera_source = 1;
+		a1=(uint16_t)(e_ctrl->memory_data[0xE0] << 8)|
+	             e_ctrl->memory_data[0xE1];
+		a2=(uint16_t)(e_ctrl->memory_data[0xEA] << 8)|
+	             e_ctrl->memory_data[0xEB];
+		starting_dac_t=(infinity_dac_t)-((a1<<7)/a2);
+		pr_err("AF infinity_dac=0x%X, macro_dac=0x%X, starting_dac=0x%X\n",infinity_dac_t,macro_dac_t,starting_dac_t);
+		pr_err("Camera eeprom 1st IMX009, GC006\n");
+	}
+	else if ((e_ctrl->memory_data[0]==0x4C)||(e_ctrl->memory_data[0]==0x6C)){
+		cci_camera_source = 2;
+	 starting_dac_t=(uint16_t)(e_ctrl->memory_data[0xD2] << 8) |
+                  e_ctrl->memory_data[0xD3];
+		pr_err("AF infinity_dac=0x%X, macro_dac=0x%X, starting_dac=0x%X\n",infinity_dac_t,macro_dac_t,starting_dac_t);
+		pr_err("Camera eeprom 2st LI005, GC006\n");
+	}
+#endif
 
 	rc = msm_camera_power_down(power_info, e_ctrl->eeprom_device_type,
 		&e_ctrl->i2c_client);

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
@@ -113,6 +113,13 @@ int msm_camera_get_dt_power_setting_data(struct device_node *of_node,
 				ps[i].seq_val = SENSOR_GPIO_STANDBY;
 			else if (!strcmp(seq_name, "sensor_gpio_vdig"))
 				ps[i].seq_val = SENSOR_GPIO_VDIG;
+#ifdef CONFIG_MACH_SONY_FLAMINGO
+// [Flamingo] Modify for Rear camera eeprom
+			else if (!strcmp(seq_name, "sensor_gpio_vio"))
+				ps[i].seq_val = SENSOR_GPIO_VIO;
+			else if (!strcmp(seq_name, "sensor_gpio_vana"))
+				ps[i].seq_val = SENSOR_GPIO_VANA;
+#endif
 			else
 				rc = -EILSEQ;
 			break;
@@ -489,7 +496,9 @@ int msm_camera_power_up(struct msm_camera_power_ctrl_t *ctrl,
 		ctrl->gpio_conf->cam_gpio_req_tbl_size, 1);
 	if (rc < 0)
 		no_gpio = rc;
-
+#ifdef CONFIG_MACH_SONY_EAGLE
+	gpio_set_value_cansleep(69,1);
+#endif
 	for (index = 0; index < ctrl->power_setting_size; index++) {
 		CDBG("%s index %d\n", __func__, index);
 		power_setting = &ctrl->power_setting[index];
@@ -642,6 +651,9 @@ int msm_camera_power_down(struct msm_camera_power_ctrl_t *ctrl,
 		sensor_i2c_client->i2c_func_tbl->i2c_util(
 			sensor_i2c_client, MSM_CCI_RELEASE);
 
+#ifdef CONFIG_MACH_SONY_EAGLE
+	gpio_set_value_cansleep(69, GPIOF_OUT_INIT_LOW);
+#endif
 	for (index = (ctrl->power_setting_size - 1); index >= 0; index--) {
 		CDBG("%s index %d\n", __func__, index);
 		power_setting = &ctrl->power_setting[index];

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_io_util.c
@@ -24,6 +24,9 @@
 #define BUFF_SIZE_128 128
 
 #undef CDBG
+
+int gpio69_count = 0;
+
 #ifdef CONFIG_MSMB_CAMERA_DEBUG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
 #else
@@ -556,7 +559,14 @@ int msm_camera_request_gpio_table(struct gpio *gpio_tbl, uint8_t size,
 			gpio_tbl[i].gpio, gpio_tbl[i].flags);
 	}
 	if (gpio_en) {
+
 		for (i = 0; i < size; i++) {
+#ifdef CONFIG_MACH_SONY_EAGLE
+			if(gpio_tbl[i].gpio == 69) {
+				gpio69_count = gpio69_count + 1;
+				CDBG("[VY5X] GPIO69_count+1 = %d\n",gpio69_count);
+			}
+#endif
 			err = gpio_request_one(gpio_tbl[i].gpio,
 				gpio_tbl[i].flags, gpio_tbl[i].label);
 			if (err) {
@@ -571,6 +581,15 @@ int msm_camera_request_gpio_table(struct gpio *gpio_tbl, uint8_t size,
 			}
 		}
 	} else {
+#ifdef CONFIG_MACH_SONY_EAGLE
+		for (i = 0; i < size; i++) {
+			if(gpio_tbl[i].gpio == 69) {
+				gpio69_count = gpio69_count-1;
+				CDBG("[VY5X] GPIO69_count-1 = %d\n",gpio69_count);
+			}
+		}
+#endif
+
 		gpio_free_array(gpio_tbl, size);
 	}
 	return rc;

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_io_util.h
@@ -21,6 +21,10 @@
 #define NO_SET_RATE -1
 #define INIT_RATE -2
 
+#ifdef CONFIG_MACH_SONY_EAGLE
+extern int gpio69_count;
+#endif
+
 void msm_camera_io_w(u32 data, void __iomem *addr);
 void msm_camera_io_w_mb(u32 data, void __iomem *addr);
 u32 msm_camera_io_r(void __iomem *addr);

include/media/msm_cam_sensor.h
@@ -539,7 +539,10 @@ enum af_camera_name {
 	ACTUATOR_MAIN_CAM_3,
 	ACTUATOR_MAIN_CAM_4,
 	ACTUATOR_MAIN_CAM_5,
-	ACTUATOR_WEB_CAM_0,
+#if defined(CONFIG_MACH_SONY_FLAMINGO) || defined(CONFIG_MACH_SONY_EAGLE)
+	ACTUATOR_MAIN_CAM_6,
+#endif
+        ACTUATOR_WEB_CAM_0,
 	ACTUATOR_WEB_CAM_1,
 	ACTUATOR_WEB_CAM_2,
 };

android-legacy/kernel

Conflicts:
	drivers/media/platform/msm/camera_v2/isp/msm_isp.h
	drivers/media/platform/msm/camera_v2/isp/msm_isp32.c
	drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
	drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
	drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.h
	drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
	include/media/msm_cam_sensor.h

drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -113,7 +113,11 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 					i2c_byte2 = (value & 0xFF00) >> 8;
 				}
 			} else {
-				i2c_byte1 = (value & 0xFF00) >> 8;
+#ifdef CONFIG_SONY_EAGLE
+					i2c_byte1 = ((value & 0x0300) >> 8) | 0xF4;
+#else
+					i2c_byte1 = (value & 0xFF00) >> 8;
+#endif
 				i2c_byte2 = value & 0xFF;
 			}
 		} else {
@@ -798,11 +802,25 @@ static int msm_actuator_close(struct v4l2_subdev *sd,
 	struct v4l2_subdev_fh *fh) {
 	int rc = 0;
 	struct msm_actuator_ctrl_t *a_ctrl =  v4l2_get_subdevdata(sd);
+#ifdef CONFIG_SONY_EAGLE
+    int rc2 = 0;
+#endif
 	CDBG("Enter\n");
 	if (!a_ctrl) {
 		pr_err("failed\n");
 		return -EINVAL;
 	}
+#ifdef CONFIG_SONY_EAGLE
+	CDBG("back to DAC 0x0000!\n");
+	rc2 = a_ctrl->i2c_client.i2c_func_tbl->i2c_write(
+		&a_ctrl->i2c_client,
+		0x00,
+		0x00,
+		MSM_CAMERA_I2C_BYTE_DATA);
+	if (rc2 < 0) {
+		pr_err("i2c write error:%d\n", rc);
+	}
+#endif
 	if (a_ctrl->act_device_type == MSM_CAMERA_PLATFORM_DEVICE) {
 		rc = a_ctrl->i2c_client.i2c_func_tbl->i2c_util(
 			&a_ctrl->i2c_client, MSM_CCI_RELEASE);

drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
@@ -24,6 +24,10 @@
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
 #endif
 
+#ifdef CONFIG_SONY_EAGLE
+int cci_camera_source = 0;
+#endif
+
 DEFINE_MSM_MUTEX(msm_eeprom_mutex);
 
 int32_t msm_eeprom_config(struct msm_eeprom_ctrl_t *e_ctrl,
@@ -217,6 +221,18 @@ int32_t read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl)
 			}
 			memptr += emap[j].mem.valid_size;
 		}
+#ifdef CONFIG_SONY_EAGLE
+		/*Bug1095,guanyi,EEPROM S*/
+		if(j==0){/*After A1 for AWB and AF,A3 A5 A7 for lsc*/
+		eb_info->i2c_slaveaddr=0xA3;
+		e_ctrl->i2c_client.cci_client->sid = eb_info->i2c_slaveaddr >> 1;
+		}
+		else{
+		eb_info->i2c_slaveaddr=eb_info->i2c_slaveaddr+2;
+		e_ctrl->i2c_client.cci_client->sid = eb_info->i2c_slaveaddr >> 1;
+		}
+		//pr_err("new id =0x%x \n",e_ctrl->i2c_client.cci_client->sid);
+#endif
 		if (emap[j].pageen.valid_size) {
 			e_ctrl->i2c_client.addr_type = emap[j].pageen.addr_t;
 			rc = e_ctrl->i2c_client.i2c_func_tbl->i2c_write(
@@ -790,6 +806,14 @@ static int32_t msm_eeprom_platform_probe(struct platform_device *pdev)
 	int32_t j = 0;
 	uint32_t temp;
 
+#ifdef CONFIG_SONY_EAGLE
+/*AF DATA S*/
+	uint16_t infinity_dac_t;
+	uint16_t macro_dac_t;
+	uint16_t starting_dac_t;
+	uint16_t a1=0;
+	uint16_t a2=0;
+#endif
 	struct msm_camera_cci_client *cci_client = NULL;
 	struct msm_eeprom_ctrl_t *e_ctrl = NULL;
 	struct msm_eeprom_board_info *eb_info = NULL;
@@ -906,6 +930,29 @@ static int32_t msm_eeprom_platform_probe(struct platform_device *pdev)
 		pr_err("%s line %d\n", __func__, __LINE__);
 	for (j = 0; j < e_ctrl->num_bytes; j++)
 		CDBG("memory_data[%d] = 0x%X\n", j, e_ctrl->memory_data[j]);
+#ifdef CONFIG_SONY_EAGLE
+		infinity_dac_t=(uint16_t)(e_ctrl->memory_data[0xD0] << 8) |
+                  e_ctrl->memory_data[0xD1];
+		macro_dac_t=(uint16_t)(e_ctrl->memory_data[0xD4] << 8) |
+                  e_ctrl->memory_data[0xD5];
+	if ((e_ctrl->memory_data[0]==0x53)||(e_ctrl->memory_data[0]==0x73)){
+		cci_camera_source = 1;
+		a1=(uint16_t)(e_ctrl->memory_data[0xE0] << 8)|
+	             e_ctrl->memory_data[0xE1];
+		a2=(uint16_t)(e_ctrl->memory_data[0xEA] << 8)|
+	             e_ctrl->memory_data[0xEB];
+		starting_dac_t=(infinity_dac_t)-((a1<<7)/a2);
+		pr_err("AF infinity_dac=0x%X, macro_dac=0x%X, starting_dac=0x%X\n",infinity_dac_t,macro_dac_t,starting_dac_t);
+		pr_err("Camera eeprom 1st IMX009, GC006\n");
+	}
+	else if ((e_ctrl->memory_data[0]==0x4C)||(e_ctrl->memory_data[0]==0x6C)){
+		cci_camera_source = 2;
+	 starting_dac_t=(uint16_t)(e_ctrl->memory_data[0xD2] << 8) |
+                  e_ctrl->memory_data[0xD3];
+		pr_err("AF infinity_dac=0x%X, macro_dac=0x%X, starting_dac=0x%X\n",infinity_dac_t,macro_dac_t,starting_dac_t);
+		pr_err("Camera eeprom 2st LI005, GC006\n");
+	}
+#endif
 
 	rc = msm_camera_power_down(power_info, e_ctrl->eeprom_device_type,
 		&e_ctrl->i2c_client);

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
@@ -113,6 +113,13 @@ int msm_camera_get_dt_power_setting_data(struct device_node *of_node,
 				ps[i].seq_val = SENSOR_GPIO_STANDBY;
 			else if (!strcmp(seq_name, "sensor_gpio_vdig"))
 				ps[i].seq_val = SENSOR_GPIO_VDIG;
+#ifdef CONFIG_MACH_SONY_FLAMINGO
+// [Flamingo] Modify for Rear camera eeprom
+			else if (!strcmp(seq_name, "sensor_gpio_vio"))
+				ps[i].seq_val = SENSOR_GPIO_VIO;
+			else if (!strcmp(seq_name, "sensor_gpio_vana"))
+				ps[i].seq_val = SENSOR_GPIO_VANA;
+#endif
 			else
 				rc = -EILSEQ;
 			break;
@@ -529,7 +536,9 @@ int msm_camera_power_up(struct msm_camera_power_ctrl_t *ctrl,
 		ctrl->gpio_conf->cam_gpio_req_tbl_size, 1);
 	if (rc < 0)
 		no_gpio = rc;
-
+#ifdef CONFIG_SONY_EAGLE
+	gpio_set_value_cansleep(69,1);
+#endif
 	for (index = 0; index < ctrl->power_setting_size; index++) {
 		CDBG("%s index %d\n", __func__, index);
 		power_setting = &ctrl->power_setting[index];
@@ -688,6 +697,9 @@ int msm_camera_power_down(struct msm_camera_power_ctrl_t *ctrl,
 		sensor_i2c_client->i2c_func_tbl->i2c_util(
 			sensor_i2c_client, MSM_CCI_RELEASE);
 
+#ifdef CONFIG_SONY_EAGLE
+	gpio_set_value_cansleep(69, GPIOF_OUT_INIT_LOW);
+#endif
 	for (index = (ctrl->power_setting_size - 1); index >= 0; index--) {
 		CDBG("%s index %d\n", __func__, index);
 		power_setting = &ctrl->power_setting[index];

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_io_util.c
@@ -24,6 +24,9 @@
 #define BUFF_SIZE_128 128
 
 #undef CDBG
+
+int gpio69_count = 0;
+
 #ifdef CONFIG_MSMB_CAMERA_DEBUG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
 #else
@@ -556,7 +559,14 @@ int msm_camera_request_gpio_table(struct gpio *gpio_tbl, uint8_t size,
 			gpio_tbl[i].gpio, gpio_tbl[i].flags);
 	}
 	if (gpio_en) {
+
 		for (i = 0; i < size; i++) {
+#ifdef CONFIG_SONY_EAGLE
+			if(gpio_tbl[i].gpio == 69) {
+				gpio69_count = gpio69_count + 1;
+				CDBG("[VY5X] GPIO69_count+1 = %d\n",gpio69_count);
+			}
+#endif
 			err = gpio_request_one(gpio_tbl[i].gpio,
 				gpio_tbl[i].flags, gpio_tbl[i].label);
 			if (err) {
@@ -571,6 +581,15 @@ int msm_camera_request_gpio_table(struct gpio *gpio_tbl, uint8_t size,
 			}
 		}
 	} else {
+#ifdef CONFIG_SONY_EAGLE
+		for (i = 0; i < size; i++) {
+			if(gpio_tbl[i].gpio == 69) {
+				gpio69_count = gpio69_count-1;
+				CDBG("[VY5X] GPIO69_count-1 = %d\n",gpio69_count);
+			}
+		}
+#endif
+
 		gpio_free_array(gpio_tbl, size);
 	}
 	return rc;

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_io_util.h
@@ -21,6 +21,10 @@
 #define NO_SET_RATE -1
 #define INIT_RATE -2
 
+#ifdef CONFIG_SONY_EAGLE
+extern int gpio69_count;
+#endif
+
 void msm_camera_io_w(u32 data, void __iomem *addr);
 void msm_camera_io_w_mb(u32 data, void __iomem *addr);
 u32 msm_camera_io_r(void __iomem *addr);

include/media/msm_cam_sensor.h
@@ -565,8 +565,10 @@ enum af_camera_name {
 	ACTUATOR_MAIN_CAM_3,
 	ACTUATOR_MAIN_CAM_4,
 	ACTUATOR_MAIN_CAM_5,
-	ACTUATOR_MAIN_CAM_6,/*Guanyi Bug904 QCT 80 header file*/
-	ACTUATOR_WEB_CAM_0,
+#if defined(CONFIG_MACH_SONY_FLAMINGO) || defined(CONFIG_SONY_EAGLE)
+	ACTUATOR_MAIN_CAM_6,
+#endif
+        ACTUATOR_WEB_CAM_0,
 	ACTUATOR_WEB_CAM_1,
 	ACTUATOR_WEB_CAM_2,
 };